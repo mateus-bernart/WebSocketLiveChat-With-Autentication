4510d6c314e2ef4b1f563a6ab1faae069212429f
Mateus Bernart <teteusao77@gmail.com>
2024-10-23 11:04:45 -0300

    refactor event register

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,8 +1,10 @@
-import registrarEventosDocumento from "./registrarEventos/registrarEventosDocumento.js";
-import registrarEventosInicio from "./registrarEventos/registrarEventosInicio.js";
+import registrarEventosCadastro from "./registrarEventos/Cadastro.js";
+import registrarEventosDocumento from "./registrarEventos/Documento.js";
+import registrarEventosInicio from "./registrarEventos/Inicio.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
+  registrarEventosCadastro(socket, io);
 });
