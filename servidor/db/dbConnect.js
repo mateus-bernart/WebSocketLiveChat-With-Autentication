5b6f168bc626dda83f08572b5abf5e68f8516a73
Mateus Bernart <teteusao77@gmail.com>
2024-10-23 11:35:18 -0300

    added helper functions to encrypt password on bd

--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -4,16 +4,18 @@ import "dotenv/config";
 const cliente = new MongoClient(process.env.CONN_STRING);
 
 let documentosColecao;
+let usuariosColecao;
 
 try {
   await cliente.connect();
 
   const db = cliente.db("alura-websockets");
   documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
 
   console.log("Conectado ao banco de dados com sucesso!");
 } catch (erro) {
   console.log(erro);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };
